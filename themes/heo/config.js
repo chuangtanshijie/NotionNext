adc6a8314009687eee0f561d0a3dd517888b3357
chuangtanshijie <154720547+chuangtanshijie@users.noreply.github.com>
2024-02-25 23:07:20 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,6 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'www.ctsj.cloudns.org' },
-    { title: '', url: 'https://docs.tangly1024.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
